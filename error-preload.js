f02975ccf5f2e656fcfb7ab2f7e37c67bcd356eb
Kinesis <yasinylmaz1903@gmail.com>
2025-12-26 06:24:41 +0300

    Remove Electron app files

--- error-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  retryLoad: (url) => ipcRenderer.send('error-retry-load', url),
-  goBack: () => ipcRenderer.send('error-go-back')
-});
